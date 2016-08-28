47faa8575ea02fdaf1422da687823982bd68bfc3
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2016-08-28 17:11:01 +0200

    Added highest_cell_index function

--- src/compiler/bounds.rs
+++ src/compiler/bounds.rs
@@ -8,3 +8,17 @@ use bfir::AstNode::*;
 use bfir::{parse, Position};
 
 pub const MAX_CELL_INDEX: usize = 99999;
+
+pub fn highest_cell_index(instrs: &[AstNode]) -> usize{
+    let (highest_index, _) = overall_movement(instrs);
+    match highest_index{
+        SaturatingInt::Number(x) => {
+            if x > MAX_CELL_INDEX as i64{
+                MAX_CELL_INDEX
+            } else{
+                x as usize
+            }
+        }
+        SaturatingInt::Max => MAX_CELL_INDEX,
+    }
+}

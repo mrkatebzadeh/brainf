244dda85ea39b7eb0bd39082f462f8d087edd3b9
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2016-08-28 18:51:27 +0200

    Added ExecutionState

--- src/compiler/execution.rs
+++ src/compiler/execution.rs
@@ -8,3 +8,10 @@ use diagnostics::Warning;
 use bounds::MAX_CELL_INDEX;
 use bounds::highest_cell_index;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ExecutionState<'a>{
+    pub start_instr: Option<&'a AstNode>,
+    pub cells: Vec<Cell>,
+    pub cell_ptr: isize,
+    pub outputs: Vec<i8>,
+}

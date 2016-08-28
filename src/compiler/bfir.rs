90630c4a638d3d5d20d9fd45123a1ceed33a09e3
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2016-08-28 15:37:10 +0200

    Added Combine trait

--- src/compiler/bfir.rs
+++ src/compiler/bfir.rs
@@ -10,3 +10,30 @@ pub struct Position{
     pub start: usize,
     pub end: usize,
 }
+
+pub trait Combine<T>{
+    fn combine(&self, T) -> T;
+}
+impl Combine<Option<Position>> for Option<Position>{
+    fn combine(&self, other: Self) -> Self{
+        match(*self, other){
+            (Some(pos1), Some(pos2)) => {
+                let (first_pos, second_pos) = if pos1.start <= pos2.start{
+                    (pos1, pos2)
+                } else{
+                    (pos2, pos1)
+                };
+
+                if first_pos.end + 1 >= second_pos.start{
+                    Some(Position{
+                        start: first_pos.start,
+                        end: second_pos.end,
+                    })
+                } else{
+                    Some(pos2)
+                }
+            }
+            _ => None,
+        }
+    }
+}
